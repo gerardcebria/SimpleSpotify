be99d1635f462ceeebc65e633c83da32231dc501
GCebria <gerardcebria@gmail.com>
2018-05-02 12:41:46 +0200

    middleware authentication

---
+++ middlewares/authenticated.js
@@ -0,0 +1,30 @@
+'use strict'
+
+var jwt = require('jwt-simple');
+var moment = require('moment');
+var secret = 'clave_secreta_curso';
+
+exports.ensureAuth = function(req, res, next){
+    if(!req.headers.authorization){
+        return res.status(403).send({message: 'La petición no tiene la cabecera de autenticacion'});
+    }
+    
+    var token = req.headers.authorization.replace(/['"]+/g, '');
+    
+    try{
+       var payload = jwt.decode(token, secret);
+       
+       if(payload.exp <= jwt.moment().unix()){
+        return res.status(401).send({message: 'token expirado'});
+       }
+
+    }catch(ex){
+        console.log(ex);
+        return res.status(404).send({message: 'token no válido'});
+
+    }
+
+    req.user = payload;
+
+    next();
+}
